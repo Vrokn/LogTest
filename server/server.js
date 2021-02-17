46fd2a25a77097c67287faf499e12ccee3bc9e03
Vrokn <61293690+Vrokn@users.noreply.github.com>
2021-02-17 11:58:51 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -71,12 +71,6 @@ const requireUser = async (req, res, next) => {
   }
 };
 
-app.get("/api/getList", (req, res) => {
-  var list = ["item1", "item2", "item3"];
-  res.json(list);
-  console.log("Sent list of items");
-});
-
 app.post("/register", async (req, res) => {
   //POST /register - crea al usuario en MongoDB.
   const name = req.body.name;
@@ -84,7 +78,7 @@ app.post("/register", async (req, res) => {
   const password = bcrypt.hashSync(req.body.password, 10);
   const user = new Users({ name: name, email: email, password: password });
   await user.save();
-  res.redirect("/");
+  res.status(200);
 });
 
 app.post("/login", async (req, res) => {
@@ -95,15 +89,15 @@ app.post("/login", async (req, res) => {
     const user = await Users.authenticate(email, password);
     if (user) {
       req.session.userId = user._id; // acá guardamos el id en la sesión
-      return res.redirect("/");
+      return res.status(200);
     } else {
-      res.redirect("/login", { error: "Wrong email or password. Try again!" });
+      res.status(500, { error: "Wrong email or password. Try again!" });
     }
   } catch (e) {
     return res.status(500).send(e);
   }
 });
-app.get("/", async (req, res) => {
+app.get("*", async (req, res) => {
   const users = await Users.find();
   res.sendFile(path.join(publicPath, "index.html"));
 });
